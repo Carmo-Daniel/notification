44fc72ed9cca5fdfa7da6e8f6b4bc66a7cffd3a0
Daniel Araujo <daniel.carmo@tjam.jus.br>
2020-02-05 11:57:15 -0400

    Conexões estabelecidas com OneSignal e Firebase.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, AlertController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { OneSignal } from '@ionic-native/onesignal/ngx';
+
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private oneSignal: OneSignal,
+    private alertCtrl: AlertController
   ) {
     this.initializeApp();
   }
@@ -22,6 +26,55 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      if(this.platform.is('cordova')){
+        this.setupPush();
+      }
     });
   }
+
+  setupPush(){
+    this.oneSignal.startInit('f88a6f55-3004-43eb-b5cb-05a0938e8b5d', '794414362582' );
+
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.None);
+
+  
+    this.oneSignal.handleNotificationReceived().subscribe(data => {
+      let msg = data.payload.body;
+      let title = data.payload.title;
+      let additionalData = data.payload.additionalData;
+      this.showAlert(title, msg, additionalData.task);
+      
+
+    });
+
+    this.oneSignal.handleNotificationOpened().subscribe(data => {
+      let additionalData = data.notification.payload.additionalData;
+      this.showAlert('Notificação aberta', 'Você já leu isto antes', additionalData.task);
+
+
+    });
+
+    this.oneSignal.endInit();
+  }
+
+  async showAlert(title, msg, task){
+    const alert = await this.alertCtrl.create({
+      header: title,
+      subHeader: msg,
+      buttons: [
+        {
+          text: `Action: ${task}`,
+          handler: () => {
+
+          }
+        }
+      ]
+    })
+    alert.present();
+  }
+
+
+
 }
+
